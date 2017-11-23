2906ac7038cb00f15792e5945f844874838c8b06
Dan Farcas <dan@farcas.me>
2017-11-23 21:11:26 +0200

    Cleaning

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { aframe } from 'aframe'; /* linter pls shut up */
 import { aframeParticleSystemComponent }  from 'aframe-particle-system-component';
+import { aframeAudioAnalyserComponent } from 'aframe-audioanalyser-component';
+
 import snow from './assets/snow.jpg';
-/*https://medium.com/@mackplevine/node-js-get-pixels-getting-pixels-at-specific-sectors-of-an-image-using-ndarray-e6d4cb285d36*/
+import sound from './assets/Plushgoolash_-_03_-_Big_Gay_Water_Fight.mp3';
 
 const w = 'white';
 const b = 'black';
@@ -34,67 +36,34 @@ const panda = [
 [w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w ]
 ].reverse();
 
-const CoolBoxesCollection = () => {
-  return panda.map((x, xIndex) => {
-    return x.map((y, yIndex) => {
-      return (
-          <a-box
-            key={`${xIndex}${yIndex}`}
-            position={`${yIndex} ${xIndex} -12`}
-            width={0.99}
-            height={0.99}
-            depth={2}
-            color={y}
-          >
-          <a-animation attribute="material.width" begin="0.99" to="0" dur="1000"></a-animation>
+const CoolBoxesCollection = () => panda.map((x, xIndex) => x.map((y, yIndex) => (
+    <a-box
+      key={`${xIndex}${yIndex}`}
+      position={`${yIndex} ${xIndex} -12`}
+      width={0.99}
+      height={0.99}
+      depth={2}
+      color={y}
+    >
+    <a-animation attribute="material.width" begin="0.99" to="0" dur="1000"></a-animation>
 
-          </a-box>
-      )
-      // console.log("x", x , "x", xIndex, "y", y, "yIndex", yIndex);
-    });
-  });
-}// coolBoxArray.map(({position, rotation, width, height, depth, color}, i) => {
-
-
-  // return (
-  //   <a-box
-  //     key={i}
-  //     position={position}
-  //     rotation={rotation}
-  //     width={width}
-  //     height={height}
-  //     depth={depth}
-  //     color={color}
-  //   ></a-box>
-  // );
-// });
+    </a-box>
+)));
 
 export default () => {
   return (
     <a-scene>
       <a-assets>
-         <img id="groundTexture" src="https://cdn.aframe.io/a-painter/images/floor.jpg" />
-       </a-assets>
-       <a-entity position="0 2.25 -15" particle-system="preset: snow; particleCount: 5000; size: 0.5; maxAge: 2"></a-entity>
+         <img crossOrigin="true" id="groundTexture" src="https://cdn.aframe.io/a-painter/images/floor.jpg" />
+         <audio crossOrigin="true" id="song" autoPlay loop src={sound}></audio>
+      </a-assets>
+
+      <a-entity position="0 2.25 -15" particle-system="preset: snow; particleCount: 5000; size: 0.5; maxAge: 2"></a-entity>
       <a-entity camera position="17 10 4" look-controls wasd-controls universal-controls />
       <a-light type="point" color="#fff" position="0 5 5"></a-light>
 
-        <a-entity geometry="primitive: plane; height: 750; width: 750" position="0 -0.5 0" rotation="-90 0 0" material={`shader: flat; src: url(${snow}); repeat: 300 300`}></a-entity>
-
-        <a-sky color="#bcd1e5">
-          <a-root>
-            <a-entity geometry="primitive: sphere;
-                                radius: 5000;
-                                segmentsWidth: 64;
-                                segmentsHeight: 64" material="shader: flat;
-                                src: url();
-                                color: #a9c8e3;
-                                fog: false" scale="-1 1 1">
-           </a-entity>
-          </a-root>
-        </a-sky>
-
-
+      <a-entity geometry="primitive: plane; height: 750; width: 750" position="0 -0.5 0" rotation="-90 0 0" material={`shader: flat; src: url(${snow}); repeat: 300 300`}></a-entity>
+      <a-sky color="#bcd1e5"></a-sky>
 
       <CoolBoxesCollection />
    </a-scene>
